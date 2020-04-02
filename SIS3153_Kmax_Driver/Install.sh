50dee7d9ea1e237306f78026fcf57f1d8128a02c
Danula Godagama <47861958+Danulagod@users.noreply.github.com>
2020-04-02 18:33:37 -0400

    Update Install.sh

--- SIS3153_Kmax_Driver/Install.sh
+++ SIS3153_Kmax_Driver/Install.sh
@@ -10,23 +10,24 @@
 #				       Install.sh
 #This shell script compile and install the SIS3153 Kmax driver components.
 #This script should be run from this directory(SIS3153_Kmax_Driver).
+#It assumes that the Kmax_Stuff folder is located in the home directory.
 #User may need to change file locations according to the linux distribution. 
 #libSIS3153.so need libusb package installed in your computer.
 #JAVA-JDK version 1.8 or above to compile SIS3153 Java class libarary 
 #*********************************************************************************************
 
-rm ./SIS3153.class ./SIS3153.jar libSIS3153.so
-
 # Compiling the SIS3153 Java class libarary 
 javac SIS3153.java
-jar -cmf manifest.txt SIS3153.jar SIS3153.class 
+jar -cmf m/home/mkovash/Kmax_Stuff/Extensions/Linux-amd64anifest.txt SIS3153.jar SIS3153.class 
 
 # Compiling the libSIS3153.so native shared library 
-gcc -Wall -fPIC -I./ \-I/usr/include \-I/usr/lib/jvm/java-11-openjdk-amd64/include \-I/usr/lib/jvm/java-11-openjdk-amd64/include/linux \-I/home/mkovash/Kmax_Stuff/Extensions/Developing/Library/Include -shared -o libSIS3153.so SIS3153.cxx /home/mkovash/Kmax_Stuff/Extensions/Developing/Library/src/* -L/lib/x86_64-linux-gnu -lusb
+gcc -Wall -fPIC -I./Include \-I/usr/include \-I/usr/lib/jvm/java-11-openjdk-amd64/include \-I/usr/lib/jvm/java-11-openjdk-amd64/include/linux -shared -o libSIS3153.so ./Src/* -L/lib/x86_64-linux-gnu -lusb
 
 # installing libraries in proper locations 
-rm /home/mkovash/Kmax_Stuff/Extensions/Linux-amd64/SIS3153.jar /home/mkovash/Kmax_Stuff/Extensions/Linux-amd64/libSIS3153.so
-cp ./SIS3153.jar ./../Linux-amd64/
-cp ./libSIS3153.so ./../Linux-amd64/
-chmod +x SIS3153.jar
+rm ~/Kmax_Stuff/Extensions/Linux-amd64/SIS3153.jar ~/Kmax_Stuff/Extensions/Linux-amd64/libSIS3153.so
+cp ./SIS3153.jar ~/Kmax_Stuff/Extensions/Linux-amd64
+chmod +x ~/Kmax_Stuff/Extensions/Linux-amd64/SIS3153.jar
+cp ./libSIS3153.so ~/Kmax_Stuff/Extensions/Linux-amd64
+cp ./SIS3153_Controller.mdr ~/Kmax_Stuff/Extensions/VME.MDRL
+
 
